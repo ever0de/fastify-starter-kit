598d7ba6803e2305b4e680073d77a8a2ac29c418
Jiseok CHOI <jiseok.dev@gmail.com>
2022-05-12 14:10:34 +0900

    Move out global error handler

--- src/app.ts
+++ src/app.ts
@@ -8,18 +8,16 @@ const app: FastifyPluginAsync<AppOptions> = async (
   fastify,
   opts,
 ): Promise<void> => {
-  fastify
-    .register(AutoLoad, {
-      dir: join(__dirname, "plugins"),
-      options: opts,
-    })
-    .after(() => {
-      fastify.setErrorHandler(async (error, _, reply) => {
-        reply.statusCode = error?.statusCode ?? 500;
+  fastify.setErrorHandler(async (error, _, reply) => {
+    reply.statusCode = error?.statusCode ?? 500;
 
-        reply.send(error);
-      });
-    });
+    return error;
+  });
+
+  fastify.register(AutoLoad, {
+    dir: join(__dirname, "plugins"),
+    options: opts,
+  });
 
   fastify.register(AutoLoad, {
     dir: join(__dirname, "routes"),
